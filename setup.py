36bd3cb06b8803ae998c5f67ede6c293e4cb952e
Vitor Vieira <jose.vieira153@gmail.com>
2018-07-06 17:40:17 +0100

    [ADD] Basic setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='metaconvexpy',
+      version='0.1',
+      description='A convex analysis tool for metabolic networks',
+      url='http://github.com/skapur/metaconvexpy',
+      author='Vítor Vieira',
+      author_email='vvieira@ceb.uminho.pt',
+      license='MIT',
+      packages=['funniest'],
+      install_requires=[
+          'numpy',
+		  ''
+      ],
+      zip_safe=False)
