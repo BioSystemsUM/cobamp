b3dc313e1caf940bf207c695406ad2393a8fefbc
vvieira <vvieira@ceb.uminho.pt>
2018-10-01 16:01:15 +0100

    [ADD] Docstrings for the utilities module

--- src/metaconvexpy/utilities/set_utils.py
+++ src/metaconvexpy/utilities/set_utils.py
@@ -1,9 +1,42 @@
 
 def is_subset(which, of_what):
+	'''
+
+	Parameters
+	----------
+	which: A set or frozenset instance to be checked as a possible subset of `of_what`
+	of_what: A set or frozenset instance
+
+	Returns a boolean indicating if `which` is a subset of `of_what`
+	-------
+
+	'''
 	return 0 < len(which & of_what) <= len(of_what)
 
 def is_identical(set1, set2):
+	'''
+
+	Parameters
+	----------
+	set1: A set of frozenset instance.
+	set2: A set of frozenset instance.
+
+	Returns a boolean indicating if both set1 and set2 are identical (contain exactly the same elements)
+	-------
+
+	'''
 	return len(set1 & set2) == len(set1) == len(set2)
 
 def has_no_overlap(set1, set2):
+	'''
+
+	Parameters
+	----------
+	set1: A set of frozenset instance.
+	set2: A set of frozenset instance.
+
+	Returns a boolean indicating if the intersection of set1 and set2 is empty.
+	-------
+
+	'''
 	return len(set1 & set2) == 0
--- src/metaconvexpy/utilities/tree_analysis.py
+++ src/metaconvexpy/utilities/tree_analysis.py
@@ -1,37 +1,98 @@
 from itertools import chain
 from collections import Counter
-import networkx as nx
+#import networkx as nx
 
-class EFMTree(object):
+class Tree(object):
+	'''
+	A simple class representing an n-ary tree as a node with one or more children nodes.
+	'''
 	def __init__(self, value, extra_info=None):
+		'''
+		Initializes the tree with no children.
+		Parameters
+		----------
+		value: A value associated with this node
+		extra_info: Additional hidden information present in the node (OPTIONAL)
+		'''
 		self.value = value
 		self.children = []
 		self.extra_info = extra_info
 
 	def get_children(self):
+		'''
+
+		Returns a list with all the children of this node.
+		-------
+
+		'''
 		return [c for c in self.children]
 
 	def add_child(self, node):
+		'''
+		Adds a node to the list of children in this node.
+		Parameters
+		----------
+		node: A <Tree> instance
+		-------
+
+		'''
 		self.children.append(node)
 
 	def is_leaf(self):
+		'''
+		Checks whether this node has no children (is a leaf)
+		Returns a boolean.
+		-------
+
+		'''
 		return self.children == []
 
 	def __eq__(self, other):
+		'''
+		Overloaded equality comparison operator. Compares the value of both nodes.
+		Parameters
+		----------
+		other
+
+		Returns a boolean
+		-------
+
+		'''
 		return self.value == other
 
 	def __repr__(self):
+		'''
+
+		Returns a representation of this node as a string containing the value and extra information.
+		-------
+
+		'''
 		return str(self.value) + '(' + str(self.extra_info) + ')'
 
 
 
 def fill_tree(tree, sets):
+	'''
+	Fills a Tree instance with data from the Iterable[set/frozenset] supplied as the argument `sets`.
+	The resulting tree will be filled in a way that each set can be retrieved by traversing from the root node `tree`
+	towards a leaf. The nodes required to travel down this path contain the values of a single set.
+	The resulting tree will not contain circular references so this should not be treated as a graph. The filling method
+	is recursive, so each child will be filled with sets contained in the parent node. Sets that have already been added
+	are removed from the original pool.
+	Elements to be added as nodes are chosen by the frequency at which they occur in the sets.
+	Parameters
+	----------
+	tree: A tree instance
+	sets: A list of set/frozenset instances.
+	-------
+
+	'''
 	if len(sets) > 0:
 		counts = Counter(chain(*(sets)))
 		if len(counts) > 0:
 			most_common, max_value = counts.most_common(1)[0]
 			#print(tab_level*"\t"+'Most common element is',most_common,'with',max_value,'hits.')
-			new_node = EFMTree(most_common, extra_info=max_value)
+			new_node = Tree(most_common, extra_info=max_value)
 			sets_containing_most_common = [setf for setf in sets if most_common in setf]
 			#print(tab_level*"\t"+str(len(sets_containing_most_common)),'contain',most_common)
 			remaining_sets = [setf for setf in sets if most_common not in setf]
@@ -44,6 +105,15 @@ def fill_tree(tree, sets):
 				fill_tree(tree, remaining_sets)
 
 def compress_linear_paths(tree):
+	'''
+	Collapses sequences of nodes contained in a Tree with only one children as a single node containing all values of
+	those nodes.
+	Parameters
+	----------
+	tree: A Tree instance.
+	-------
+
+	'''
 	if tree.is_leaf():
 		pass
 	else:
@@ -57,6 +127,16 @@ def compress_linear_paths(tree):
 			compress_linear_paths(child)
 
 def ignore_compressed_nodes_by_size(tree, size):
+	'''
+	Modifies the values of a tree's children that have been previously compressed with the <compress_linear_paths>
+	function if they contain more than a certain number of elements. The node's value is changed to "REMAINING".
+	Parameters
+	----------
+	tree: A Tree instance
+	size: An integer with the size threshold
+	-------
+
+	'''
 	for child in tree.children:
 		if isinstance(child.value, list) and len(child.value) > size:
 			child.value = 'REMAINING'
@@ -65,6 +145,19 @@ def ignore_compressed_nodes_by_size(tree, size):
 			ignore_compressed_nodes_by_size(child, size)
 
 def probabilistic_tree_prune(tree, target_level, current_level=0, cut_leaves = False, name_separator=' and '):
+	'''
+	Cuts a tree's nodes under a certain height (`target_level`) and converts ensuing nodes into a single one whose value
+	represents the relative frequency of an element in the nodes below. Requires values on the extra_info field.
+	Parameters
+	----------
+	tree: A Tree instance
+	target_level: An int representing the level at which the tree will be cut
+	current_level: The current level of the tree (int). Default is 0 for root nodes.
+	cut_leaves: A boolean indicating whether the node at the target level is excluded or displays probabilities.
+	name_separator: Separator to use when representing multiple elements
+	-------
+
+	'''
 	if current_level < target_level:
 		if target_level == current_level and cut_leaves:
 			tree.children = []
@@ -78,6 +171,19 @@ def probabilistic_tree_prune(tree, target_level, current_level=0, cut_leaves = F
 	return target_level == current_level
 
 def probabilistic_tree_compression(tree, data=None, total_count=None, name_separator=' and '):
+	'''
+	Compresses a node and subsequent children by removing them and modifying the value to a dictionary with the relative
+	frequency of each element in the subsequent nodes. Requires values on the extra_info field.
+
+	Parameters
+	----------
+	tree: A Tree instance
+	data: Local count if not available in extra_info
+	total_count: Total amount of sets if not available in extra_info
+	name_separator: Separator to use when representing multiple elements
+	-------
+
+	'''
 	if data is None and total_count is None:
 		total_count = int(tree.extra_info)
 		data = {name_separator.join(tree.value) if isinstance(tree.value, list) else tree.value: 1}
@@ -97,6 +203,16 @@ def probabilistic_tree_compression(tree, data=None, total_count=None, name_separ
 
 
 def pretty_print_tree(tree, write_path=None):
+	'''
+	Parameters
+	----------
+	tree: A Tree instance
+	write_path: Path to store a text file. Use None if the string is not to be stored in a file.
+
+	Returns a text representation of a Tree instance along with its children.
+	-------
+
+	'''
 	buffer = []
 
 	def __pretty_print_tree(tree, buffer, overhead, final_node=True):
@@ -118,11 +234,29 @@ def pretty_print_tree(tree, write_path=None):
 	return res
 
 def apply_fx_to_all_node_values(tree, fx):
+	'''
+	Applies a function to all nodes below the tree, modifying their value to its result.
+	Parameters
+	----------
+	tree: A Tree instance
+	fx: A function to apply
+	-------
+
+	'''
 	tree.value = fx(tree.value)
 	for child in tree.children:
 		apply_fx_to_all_node_values(child, fx)
 
 def find_all_tree_nodes(tree):
+	'''
+	Parameters
+	----------
+	tree: A Tree instance.
+
+	Returns a list of all nodes below a node
+	-------
+
+	'''
 	def __find_all_tree_nodes(tree, it):
 		it.append(tree)
 		for child in tree.children:
@@ -134,10 +268,20 @@ def find_all_tree_nodes(tree):
 
 
 def merge_duplicate_nodes(tree):
+	'''
+	Merges all nodes with similar values, replacing every instance reference of all nodes with the same object if its
+	value is identical
+
+	Parameters
+	----------
+	tree: A Tree instance
+	-------
+
+	'''
 	all_nodes = find_all_tree_nodes(tree)
 	conv_key = lambda x: str(sorted(x) if isinstance(x, list) else x)
 	unique_keys = [conv_key(k.value) for k in all_nodes]
-	unique_node_map = {k:EFMTree(k) for k in unique_keys}
+	unique_node_map = {k:Tree(k) for k in unique_keys}
 
 	def __merge_duplicate_nodes(tree):
 		new_children = []
@@ -152,24 +296,24 @@ def merge_duplicate_nodes(tree):
 	__merge_duplicate_nodes(tree)
 
 
-def populate_nx_graph(tree, G, previous=None, name_separator='\n', unique_nodes=True, node_dict=None):
-	if node_dict is None:
-		node_dict = {}
-	if unique_nodes:
-		node_value_key = name_separator.join(tree.value) if type(tree.value) == list else str(tree.value)
-		node_value = node_value_key
-		if node_value_key not in node_dict.keys():
-			node_dict[node_value_key] = 1
-			node_value = node_value + "_" + '0'
-		else:
-			node_value = node_value + "_" + str(node_dict[node_value])
-			node_dict[node_value_key] += 1
-	else:
-		node_value = tree.value
-		node_value_key = tree.value
-	if previous != None:
-		previous_node, previous_key = previous
-		G.add_edge(previous_node, node_value)
-	if not tree.is_leaf():
-		for child in tree.children:
-			populate_nx_graph(child, G, previous=(node_value,node_value_key), name_separator=name_separator, unique_nodes=unique_nodes, node_dict=node_dict)
+# def populate_nx_graph(tree, G, previous=None, name_separator='\n', unique_nodes=True, node_dict=None):
+# 	if node_dict is None:
+# 		node_dict = {}
+# 	if unique_nodes:
+# 		node_value_key = name_separator.join(tree.value) if type(tree.value) == list else str(tree.value)
+# 		node_value = node_value_key
+# 		if node_value_key not in node_dict.keys():
+# 			node_dict[node_value_key] = 1
+# 			node_value = node_value + "_" + '0'
+# 		else:
+# 			node_value = node_value + "_" + str(node_dict[node_value])
+# 			node_dict[node_value_key] += 1
+# 	else:
+# 		node_value = tree.value
+# 		node_value_key = tree.value
+# 	if previous != None:
+# 		previous_node, previous_key = previous
+# 		G.add_edge(previous_node, node_value)
+# 	if not tree.is_leaf():
+# 		for child in tree.children:
+# 			populate_nx_graph(child, G, previous=(node_value,node_value_key), name_separator=name_separator, unique_nodes=unique_nodes, node_dict=node_dict)
