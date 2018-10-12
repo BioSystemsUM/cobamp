183e9c82c05a45e1537d9afd915c815d1c0d79f6
skapur <jose.vieira153@gmail.com>
2018-10-12 17:35:15 +0100

    [ADD] Working test and class

--- src/metaconvexpy/connectors/cobrapy.py
+++ src/metaconvexpy/connectors/cobrapy.py
@@ -6,19 +6,25 @@ import metaconvexpy.convex_analysis.efm_enumeration.kshortest_efm_properties as 
 
 import numpy as np
 
-class KShortestEnumerator(object):
+
+class KShortestEnumeratorWrapper(object):
 	"""
 	An abstract class for methods involving the K-shortest EFM enumeration algorithm
 	"""
-	ITERATIVE = 'kse_iterative'
-	POPULATE = 'kse_populate'
+	ALGORITHM_TYPE_ITERATIVE = 'kse_iterative'
+	ALGORITHM_TYPE_POPULATE = 'kse_populate'
 
 	__alg_to_prop_name = {
-		ITERATIVE: kp.K_SHORTEST_OPROPERTY_MAXSOLUTIONS,
-		POPULATE:kp.K_SHORTEST_OPROPERTY_MAXSIZE
+		ALGORITHM_TYPE_ITERATIVE: kp.K_SHORTEST_OPROPERTY_MAXSOLUTIONS,
+		ALGORITHM_TYPE_POPULATE:kp.K_SHORTEST_OPROPERTY_MAXSIZE
 	}
 
-	def __init__(self, model, algorithm_type, stop_criteria, forced_solutions=None, excluded_solutions=None):
+	__alg_to_alg_name = {
+		ALGORITHM_TYPE_ITERATIVE: kp.K_SHORTEST_METHOD_ITERATE,
+		ALGORITHM_TYPE_POPULATE: kp.K_SHORTEST_METHOD_POPULATE
+	}
+
+	def __init__(self, model, algorithm_type=ALGORITHM_TYPE_POPULATE, stop_criteria=1, forced_solutions=None, excluded_solutions=None):
 		"""
 
 		Parameters
@@ -31,11 +37,17 @@ class KShortestEnumerator(object):
 		"""
 
 		self.__model = model
-		self.__model_reader = COBRAModelObjectReader(model)
+		if model.__module__ in model_readers.keys():
+			self.model_reader = model_readers[model.__module__](model)
+		else:
+			raise TypeError("The `model` instance is not currently supported by metaconvexpy. Currently available readers are: "+str(list(model_readers.keys())))
+
 		self.__algo_properties = kp.KShortestProperties()
+		self.__algo_properties[kp.K_SHORTEST_MPROPERTY_METHOD] = self.__alg_to_alg_name[algorithm_type]
 		self.__algo_properties[self.__alg_to_prop_name[algorithm_type]] = stop_criteria
 		self.__forced_solutions = forced_solutions
 		self.__excluded_solutions = excluded_solutions
+		self.__setup_algorithm()
 
 	def __setup_algorithm(self):
 		'''
@@ -45,60 +57,61 @@ class KShortestEnumerator(object):
 		'''
 		self.__algo = KShortestEFMAlgorithm(self.__algo_properties, False)
 
-	def __validate(self):
-		"""
-		Returns: Boolean value (True if parameters are correctly set up)
-
-		"""
-		pass
-
-	def __get_linear_system(self):
-		"""
-		Implemented by subclass
-		Returns: A KShortestCompatibleLinearSystem instance
-
-		"""
-
 	def __get_forced_solutions(self):
 		"""
 		Returns: A KShortestCompatibleLinearSystem instance
 
 		"""
+		return self.__forced_solutions
 
 	def __get_excluded_solutions(self):
 		"""
 		Returns: A KShortestCompatibleLinearSystem instance
 
 		"""
+		return self.__excluded_solutions
 
-	def __get_enumerator(self):
-		self.__algo.get_enumerator(
-			linear_system=self.__get_linear_system(),
+	def get_linear_system(self):
+		"""
+
+		Returns
+		-------
+
+		"""
+		return
+
+	def get_enumerator(self):
+		return self.__algo.get_enumerator(
+			linear_system=self.get_linear_system(),
 			forced_sets=self.__get_forced_solutions(),
 			excluded_sets=self.__get_excluded_solutions())
 
 
-class KShortestEFMEnumerator(KShortestEnumerator):
+class KShortestEFMEnumeratorWrapper(KShortestEnumeratorWrapper):
 
 	def __get_linear_system(self, *args, **kwargs):
-		system = IrreversibleLinearSystem(S, irrev, non_consumed, consumed, produced)
-		return system
+		return IrreversibleLinearSystem(S, irrev, non_consumed, consumed, produced)
 
-class KShortestMCSEnumerator(KShortestEnumerator):
 
-	def __init__(self, *args, target_flux_space, target_yield_space, **kwargs):
-		super().__init__(*args, **kwargs)
-		converted_fbs = [DefaultFluxbound.from_tuple(self.__model_reader.convert_constraint_ids(t, False)) for t in target_flux_space]
-		converted_ybs = [DefaultYieldbound.from_tuple(self.__model_reader.convert_constraint_ids(t, True)) for t in target_yield_space]
+class KShortestMCSEnumeratorWrapper(KShortestEnumeratorWrapper):
+
+	def __init__(self, model, target_flux_space_dict, target_yield_space_dict, **kwargs):
+		super().__init__(model, **kwargs)
+
+
+		target_flux_space = [tuple([k]) + tuple(v) for k,v in target_flux_space_dict.items()]
+		target_yield_space = [k + tuple(v) for k,v in target_yield_space_dict.items()]
+		converted_fbs = [DefaultFluxbound.from_tuple(self.model_reader.convert_constraint_ids(t, False)) for t in target_flux_space]
+		converted_ybs = [DefaultYieldbound.from_tuple(self.model_reader.convert_constraint_ids(t, True)) for t in target_yield_space]
 		self.__ip_constraints = converted_fbs + converted_ybs
 
 	def __materialize_intv_problem(self):
-		return InterventionProblem(self.__model_reader.S).generate_target_matrix(self.__ip_constraints)
+		return InterventionProblem(self.model_reader.S).generate_target_matrix(self.__ip_constraints)
 
-	def __get_linear_system(self):
+	def get_linear_system(self):
 		T, b = self.__materialize_intv_problem()
-		system = DualLinearSystem(self.__model_reader.S, self.__model_reader.irrev_bool, T, b)
-		return system
+		return DualLinearSystem(self.model_reader.S, self.model_reader.irrev_index, T, b)
+
 
 class COBRAModelObjectReader(object):
 	def __init__(self, model):
@@ -107,35 +120,35 @@ class COBRAModelObjectReader(object):
 		self.__rx_instances = [model.reactions.get_by_id(rx) for rx in self.__r_ids]
 
 		self.S = self.get_stoichiometric_matrix()
-		self.irrev_bool = self.get_irreversibilities()
+		self.irrev_bool = self.get_irreversibilities(False)
 		self.irrev_index = self.get_irreversibilities(True)
-		self.lb, self.ub = tuple(zip(*self.get_model_bounds()))
+		self.lb, self.ub = tuple(zip(*self.get_model_bounds(False)))
 		self.bounds_dict = self.get_model_bounds(True)
 
 
 	def get_stoichiometric_matrix(self):
-		S = np.zeros((self.__r_ids, self.__m_ids))
+		S = np.zeros((len(self.__m_ids), len(self.__r_ids)))
 		for i,r_id in enumerate(self.__r_ids):
-			for metab, coef in self.__model.reactions.get_by_id(r_id).items()
-				S[self.__m_ids.index(metab.id)] = coef
+			for metab, coef in self.__model.reactions.get_by_id(r_id).metabolites.items():
+				S[self.__m_ids.index(metab.id), i] = coef
 
 		return S
 
 	def get_model_bounds(self, as_dict):
-		bounds = [(r.lb, r.ub) for r in self.__rx_instances]
+		bounds = [r.bounds for r in self.__rx_instances]
 		if as_dict:
 			return dict(zip(self.__r_ids, bounds))
 		else:
 			return tuple(bounds)
 
 	def get_irreversibilities(self, as_index):
-		irrev = [not r.reversible for r in self.__rx_instances]
+		irrev = [not r.reversibility for r in self.__rx_instances]
 		if as_index:
 			irrev = list(np.where(irrev)[0])
 		return irrev
 
 	def get_metabolite_and_reactions_ids(self):
-		return tuple([[x.id for x in lst] for lst in (model.reactions, model.metabolites)])
+		return tuple([[x.id for x in lst] for lst in (self.__model.reactions, self.__model.metabolites)])
 
 	def reaction_id_to_index(self, id):
 		return self.__r_ids.index(id)
@@ -143,12 +156,13 @@ class COBRAModelObjectReader(object):
 	def metabolite_id_to_index(self, id):
 		return self.__m_ids.index(id)
 
-	def convert_constraint_ids(self, tup, yield_constaint):
-		if yield_constaint:
+	def convert_constraint_ids(self, tup, yield_constraint):
+		if yield_constraint:
 			constraint = tuple(list(map(self.reaction_id_to_index,tup[:2])) + list(tup[2:]))
 		else:
 			constraint = tuple([self.reaction_id_to_index(tup[0])] + list(tup[1:]))
 		return constraint
 
-
-model = cobra.io.sbml3.read_sbml_model("/home/skapur/MEOCloud/Projectos/DeYeast/Models/iMM904/iMM904_peroxisome.xml")
+model_readers = {
+	'cobra.core.model': COBRAModelObjectReader
+}
---
+++ tests/cobra_wrapper_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+class COBRAWrapperTest(unittest.TestCase):
+	def setUp(self):
+		from cobra.io.sbml3 import read_sbml_model
+		import metaconvexpy.connectors.cobrapy as connector
+
+		model = read_sbml_model("/home/skapur/MEOCloud/Projectos/DeYeast/Models/iMM904/iMM904_peroxisome.xml")
+
+		flux_space = {
+			'EX_glc_e_' : (-1.15,-1.15),
+			'EX_o2_e_' : (None, 0),
+			'ATPM' : (8.39, 8.39)
+		}
+		yield_space = {
+			('EX_succ_e_', 'EX_glc_e_') : (None, -0.001, None)
+		}
+
+		algorithm = connector.KShortestMCSEnumeratorWrapper(model, flux_space, yield_space)
+		enumerator = algorithm.get_enumerator()
+		solutions = next(enumerator)
+		sol = solutions[0]
+		sol.attribute_dict('signed_indicator_map')
+if __name__ == '__main__':
+	COBRAWrapperTest().setUp()
