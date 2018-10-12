6af5b52aca41aa9549cf23b971175141981093cd
skapur <jose.vieira153@gmail.com>
2018-10-12 18:24:13 +0100

    [ADD] Plotting code [NOT THAT USEFUL]

--- examples/GBconsensus_resources/gb_plots.py
+++ examples/GBconsensus_resources/gb_plots.py
@@ -2,31 +2,31 @@ from metaconvexpy.utilities.file_utils import read_pickle
 from metaconvexpy.utilities.tree_analysis import *
 from itertools import chain
 import matplotlib.pyplot as plt
+import networkx as nx
 
 def generate_efm_results_tree(efm_sets, ignore_greater_than=10, pruning_level=6, merge_dupes=False):
-	root = EFMTree('ROOT')
+	root = Tree('ROOT')
 	fill_tree(root, efm_sets)
 	compress_linear_paths(root)
 	if ignore_greater_than:
 		ignore_compressed_nodes_by_size(root, ignore_greater_than)
 	apply_fx_to_all_node_values(root, lambda x: '\n'.join(sorted(x)) if isinstance(x, list) else x if x is not None else "None")
 	if pruning_level:
-		probabilistic_tree_prune(root, target_level=6, cut_leaves=True, name_separator='\n')
+		probabilistic_tree_prune(root, target_level=pruning_level, cut_leaves=True, name_separator='\n')
 	compress_linear_paths(root)
 	if merge_dupes:
 		merge_duplicate_nodes(root)
-
 	return root
 
-def draw_graph(root, write_path):
-	G = nx.OrderedDiGraph()
-	populate_nx_graph(root, G, unique_nodes=False)
-
+def draw_graph(root, write_path, unique=False):
+	G = nx.DiGraph()
+	populate_nx_graph(root, G, unique_nodes=unique)
+	print('NetworkX recognizes this as a tree?',nx.is_tree(G))
 	pos = nx.nx_pydot.graphviz_layout(G)
-	plt.figure(figsize=(20,15))
-	nx.draw_networkx_nodes(G, pos, node_size=1)
+	plt.figure(figsize=(50,40))
+	nx.draw_networkx_nodes(G, pos, node_size=0)
 	nx.draw_networkx_edges(G, pos, alpha=0.5, arrowsize=10)
-	nx.draw_networkx_labels(G, pos, font_size=12, font_color='red')
+	nx.draw_networkx_labels(G, pos, font_size=18, font_color='red')
 	if isinstance(write_path, str):
 		plt.savefig(write_path)
 
@@ -40,8 +40,8 @@ if __name__ == '__main__':
 	tree = generate_efm_results_tree(
 		efm_sets=efm_sets,
 		ignore_greater_than=10,
-		pruning_level=6,
+		pruning_level=10,
 		merge_dupes=False
 	)
 
-	draw_graph(tree, 'test_graph.pdf')
+	draw_graph(tree, 'test_graph.pdf', unique=False)
