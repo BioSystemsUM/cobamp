b03af546e7f79e4c3c825f6c4ee3ebc6cecaf1f8
skapur <jose.vieira153@gmail.com>
2018-12-20 18:56:49 +0000

    [FIX] KShortestProperties dictionaries

--- src/cobamp/algorithms/kshortest.py
+++ src/cobamp/algorithms/kshortest.py
@@ -40,10 +40,10 @@ K_SHORTEST_METHOD_POPULATE = "POPULATE"
 K_SHORTEST_OPROPERTY_MAXSIZE = 'MAXSIZE'
 K_SHORTEST_OPROPERTY_MAXSOLUTIONS = "MAXSOLUTIONS"
 
-__kshortest_mandatory_properties = {
+kshortest_mandatory_properties = {
 	K_SHORTEST_MPROPERTY_METHOD: [K_SHORTEST_METHOD_ITERATE, K_SHORTEST_METHOD_POPULATE]}
 
-__kshortest_optional_properties = {
+kshortest_optional_properties = {
 	K_SHORTEST_OPROPERTY_MAXSIZE: lambda x: x > 0 and isinstance(x, int),
 	K_SHORTEST_OPROPERTY_MAXSOLUTIONS: lambda x: x > 0 and isinstance(x, int)
 }
@@ -59,7 +59,7 @@ class KShortestProperties(PropertyDictionary):
 	"""
 
 	def __init__(self):
-		super().__init__(__kshortest_mandatory_properties, __kshortest_optional_properties)
+		super().__init__(kshortest_mandatory_properties, kshortest_optional_properties)
 
 class KShortestEnumerator(object):
 	"""
