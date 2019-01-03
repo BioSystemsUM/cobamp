2b1286f5e392b27e8161db4962ece04970570dbc
skapur <jose.vieira153@gmail.com>
2019-01-03 15:46:38 +0000

    [FIX] Trying another path for sphinx...

--- docs_gen/source/conf.py
+++ docs_gen/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/cobamp/'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 
 
 # -- Project information -----------------------------------------------------
