5ac46876b94e0e33a9e057089e2f3a8565773fbc
skapur <jose.vieira153@gmail.com>
2019-04-29 17:22:28 +0100

    [UPDATE] setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cobamp',
-    version = '0.1.0rc1',
+    version = '0.1.0',
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = ["numpy",
