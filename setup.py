45c26650eb45071272c16212dba0c6efd08cb00b
skapur <jose.vieira153@gmail.com>
2019-06-07 12:23:33 +0100

    [DOC] Prepare changelog and setup files for 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cobamp',
-    version = '0.1.0',
+    version = '0.1.1',
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = ["numpy",
