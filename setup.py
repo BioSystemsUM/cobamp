20944be14a3a4f0092eef670d41cc0e010a45c00
skapur <jose.vieira153@gmail.com>
2019-10-09 11:27:21 +0100

    [RELEASE] Version 0.1.3 setup and logs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cobamp',
-    version = '0.1.2',
+    version = '0.1.3',
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = ["numpy",
