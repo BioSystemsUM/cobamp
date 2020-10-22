65db55fab43750cc882ca58ba11c41672fad2fff
Vítor Vieira <skapur@users.noreply.github.com>
2020-10-22 11:23:33 +0100

    [FIX] Moved imports to top-level

--- src/cobamp/wrappers/external_wrappers.py
+++ src/cobamp/wrappers/external_wrappers.py
@@ -1,11 +1,12 @@
 import warnings
+from .cobra import COBRAModelObjectReader
+from .framed import FramedModelObjectReader
+from .cobamp import CobampModelObjectReader
+from .core import MatFormatReader, AbstractObjectReader
+
+from ..wrappers import get_model_reader, model_readers
 
 warnings.warn(
     '''\nThe wrappers.external_wrappers module will be deprecated in a future release in favour of the wrappers module. 
     Available ModelObjectReader classes can still be loaded using cobamp.wrappers.<class>. An appropriate model 
     reader can also be created using the get_model_reader function on cobamp.wrappers''')
-
-from .cobra import COBRAModelObjectReader
-from .framed import FramedModelObjectReader
-from .core import CobampModelObjectReader, MatFormatReader, AbstractObjectReader
-from ..wrappers import get_model_reader, model_readers
